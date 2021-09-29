999e4b0428dc7e0cbffdfced95258494ca5935d2
Mareks Zeile <mzeile23@mzeile23.kentdenver.org>
2021-09-29 14:37:47 -0600

    Changed instance data into generic type T. Set up the 1 variable constructor

--- src/NodeProject.java
+++ src/NodeProject.java
@@ -8,11 +8,16 @@ import javax.xml.soap.Node;
  Collaboration:
  Date: 9/29/21
  **/
-public class NodeProject {
+public class NodeProject <T>{
     //instance data
     //two node project instances to store pointers
     private NodeProject node1;
     private NodeProject node2;
     //store a way to assign values to the nodes
-    private Object value;
+    private T value;
+
+    //1 variable constructor
+    public NodeProject(T value){
+        this.value = value;
+    }
 }
