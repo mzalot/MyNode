c7ac7cbc2b1ac6a9b67d42903f63f913b9156ed1
Mareks Zeile <mzeile23@mzeile23.local>
2021-10-02 15:37:39 -0600

    All accessors and mutators set up. The toString and equals methods were set up. Everything is finished but testing the code

--- src/NodeProject.java
+++ src/NodeProject.java
@@ -11,7 +11,9 @@ import javax.xml.soap.Node;
 public class NodeProject <T>{
     //instance data
     //two node project instances to store pointers
+    //parent node
     private NodeProject node1;
+    //child node
     private NodeProject node2;
     //store a way to assign values to the nodes
     private T value;
@@ -28,5 +30,49 @@ public class NodeProject <T>{
         this.value = value;
         this.node1 = node1;
         this.node2 = node2;
+    }
+    //accessors and mutators
+
+    //all mutators
+    public void setValue(T value){
+        this.value = value;
+    }
+    public void setNode1(NodeProject node1){
+        this.node1 = node1;
+        makeChild(node1);
+    }
+    public void setNode2(NodeProject node2){
+        this.node2 = node2;
+        makeParent(node2);
+    }
+
+    //make a child node
+    private void makeChild(NodeProject node1){
+        this.node2 = node1;
+    }
+    //make a parent node
+    private void makeParent(NodeProject node2){
+        this.node1 = node2;
+    }
+    //accessors
+    public T getValue(){
+        return value;
+    }
+    public NodeProject getNode1(){
+        return node1;
+    }
+    public NodeProject getNode2(){
+        return node2;
+    }
+
+    //toString method
+    public String toString(){
+        return "Node: " + value.toString();
+    }
+
+    //.equals method
+    public boolean equals(NodeProject node){
+        boolean b = this.value.equals(node.getValue());
+        return b;
     }
 }
